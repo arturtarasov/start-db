ad19e6efca18a0288b1aa4a42f5d3dee00eed515
Artur <artur-tarasov@mail.ru>
2019-10-04 23:53:24 +0300

    added components, api, servie, loader, pages, styles

---
+++ src/components/random-planet/random-planet.js
@@ -0,0 +1,79 @@
+import React, { Component } from 'react'
+import SwapiService from '../../services/swapi-service'
+import Spinner from '../spinner'
+import './random-planet.css'
+
+export default class RandomPlanet extends Component {
+  swapiService = new SwapiService()
+
+  state = {
+    planet: {},
+    loading: true
+  }
+
+  constructor() {
+    super()
+    this.updatePlanet()
+  }
+
+  onPlanetLoaded = (planet) => {
+    this.setState({
+      planet,
+      loading: false
+    })
+  }
+
+  onError = (error) => {
+
+  }
+
+  updatePlanet() {
+    const id = Math.floor(Math.random() * 25) + 2
+    this.swapiService
+      .getPlanet(id)
+      .then(this.onPlanetLoaded)
+      .catch(this.onError)
+  }
+
+  render() {
+    const { planet, loading} = this.state
+
+    const spinner = loading ? <Spinner /> : null
+    const content = !loading ? <PlanetView planet={planet}/>: null
+
+    return (
+      <div className="random-planet jumbotron rounded">
+        {spinner}
+        {content}
+      </div>
+
+    );
+  }
+}
+
+const PlanetView = ({planet}) => {
+  const { id, name, population, rotationPeriod, diameter } = planet
+  return (
+    <React.Fragment>
+      <img className="planet-image" alt="random-planet"
+          src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`} />
+        <div>
+          <h4>{name}</h4>
+          <ul className="list-group list-group-flush">
+            <li className="list-group-item">
+              <span className="term">Population</span>
+              <span> {population}</span>
+            </li>
+            <li className="list-group-item">
+              <span className="term">Rotation Period</span>
+              <span>{rotationPeriod}</span>
+            </li>
+            <li className="list-group-item">
+              <span className="term">Diametr</span>
+              <span>{diameter}</span>
+            </li>
+          </ul>
+        </div>
+    </React.Fragment>
+  )
+}
